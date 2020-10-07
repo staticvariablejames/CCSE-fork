af4c6bf485ce8bc91347cf2fc891a03c1ca7fe24
Static Variable James <staticvariablejames@gmail.com>
2020-10-06 21:45:26 -0500

    test.js: Add reference to the CCtest repository.

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+// See https://github.com/staticvariablejames/CCtest
+
 function ccseTest() {
 	let testSave = "eyJ2ZXJzaW9uIjoiMi4wMTciLCJBY2hpZXZlbWVudHMiOnt9LCJVcGdyYWRlcyI6e30sIkJ1aWxkaW5ncyI6e30sIkJ1ZmZzIjp7fSwiU2Vhc29ucyI6e30sIk90aGVyTW9kcyI6eyJ0ZXN0Ijp7fX19%21END%21";
 	/* Corresponds to {"version":"2.017","Achievements":{},"Upgrades":{},"Buildings":{},"Buffs":{},"Seasons":{},"OtherMods":{"test":{}}}
